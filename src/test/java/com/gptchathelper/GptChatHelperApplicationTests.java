9633b0aa1c89202d17b647e2a458d01b7a60c2c3
FireNirva <zhengjianxuan111@gmail.com>
2023-12-02 23:17:40 -0500

    feat: Add ChatGPT response generation
    Implemented a new feature in the chat application that allows users to
    generate responses from ChatGPT. This includes modifications to the backend
    Spring controller to handle 'GENERATE_RESPONSE' message types and send requests
    to OpenAI's API. On the frontend, a new button 'Generate Response' has been
    added to trigger this functionality and handle the display of generated messages.

--- src/test/java/com/gptchathelper/GptChatHelperApplicationTests.java
+++ src/test/java/com/gptchathelper/GptChatHelperApplicationTests.java
@@ -1,13 +1,34 @@
 package com.gptchathelper;
 
+import com.gptchathelper.service.ChathistoryImpl;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class GptChatHelperApplicationTests {
 
+	@Autowired
+    ChathistoryImpl chathistoryService;
+
 	@Test
 	void contextLoads() {
+		chathistoryService.insertChathistory("1", "2", "3", "4");
+	}
+
+    @Test
+    void deletehistoryById() {
+		chathistoryService.deleteChathistoryByID("1");
+	}
+
+	@Test
+	void deleteAllChathistory() {
+		chathistoryService.deleteAllChathistory();
+	}
+
+	@Test
+	void findLatestMessage() {
+		System.out.println(chathistoryService.getLatestMessageContent());
 	}
 
 }
